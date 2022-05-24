05ce5c102b77cc25942efda7d2296064a0cfa071
John Fieweger <john.fieweger98@gmail.com>
2022-05-24 09:19:37 +0200

    backlog design completed

--- js/backlog.js
+++ js/backlog.js
@@ -2,14 +2,6 @@ function loadBacklog() {
 
     $('backlogContent').innerHTML = '';
     $('backlogContent').innerHTML = /*html*/ `
-
-            <div class="">
-                <table>
-                    <tr id="table-header">
-                        <th class="assignedTo">ASSIGNED TO</th>
-                        <th class="category">CATEGORY</th>
-                        <th class="details">DETAILS</th>
-                    </tr>
                     <tr class="">
                         <td class="">
                             <img src="img/sebastian.jpg">
@@ -20,27 +12,5 @@ function loadBacklog() {
                         <td class="">Marketing</td>
                         <td class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis nihil aspernatur ipsam magni, quod corrupti reiciendis aliquam similique fuga praesentium, beatae laudantium deserunt eveniet? Repellat doloremque veritatis enim. Esse, magni.</td>
                     </tr>
-                    <tr class="">
-                        <td class="">
-                            <img src="img/sebastian.jpg">
-                            <div class="name">
-                                <span>Sebastian Zimmermann</span>
-                            </div>
-                        </td>
-                        <td class="">Development</td>
-                        <td class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis nihil aspernatur ipsam magni, quod corrupti reiciendis aliquam similique fuga praesentium, beatae laudantium deserunt eveniet? Repellat doloremque veritatis enim. Esse, magni.</td>
-                    </tr>
-                    <tr class="">
-                        <td class="">
-                            <img src="img/sebastian.jpg">
-                            <div class="name">
-                                <span>Sebastian Zimmermann</span>
-                            </div>
-                        </td>
-                        <td class="">Sales</td>
-                        <td class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis nihil aspernatur ipsam magni, quod corrupti reiciendis aliquam similique fuga praesentium, beatae laudantium deserunt eveniet? Repellat doloremque veritatis enim. Esse, magni.</td>
-                    </tr>
-                </table>
-            </div>
     `;
 }
